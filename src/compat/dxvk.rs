97b1b1d7b2e2c44ab1b1d1864dd2291dda081dde
TukanDev <contact@tukandev.com>
2025-05-17 18:44:49 +0200

    Add dxvk new param and fix xxmi github api pulling
    Signed-off-by: TukanDev <contact@tukandev.com>

--- src/compat/dxvk.rs
+++ src/compat/dxvk.rs
@@ -5,9 +5,18 @@ use crate::compat::Compat;
 
 #[cfg(feature = "compat")]
 impl Compat {
-    pub fn add_dxvk(wine: String, prefix: String, dxvk: String) -> Result<bool, String> {
+    pub fn add_dxvk(wine: String, prefix: String, dxvk: String, repair_dlls: bool) -> Result<bool, String> {
         let wine = Wine::from_binary(wine).with_prefix(prefix).with_arch(WineArch::Win64);
-        let wp = wine.install_dxvk(dxvk, InstallParams::default());
+        let ip = InstallParams {
+            dxgi: true,
+            d3d9: true,
+            d3d10core: true,
+            d3d11: true,
+            repair_dlls,
+            arch: Default::default(),
+        };
+
+        let wp = wine.install_dxvk(dxvk, ip);
         if wp.is_ok() {
             Ok(true)
         } else {
--- src/download/extras.rs
+++ src/download/extras.rs
@@ -3,6 +3,7 @@ use std::path::Path;
 use crate::download::Extras;
 use crate::utils::{get_codeberg_release, get_github_release, get_tukanrepo_release};
 use crate::utils::downloader::Downloader;
+use crate::utils::github_structs::Asset;
 
 #[cfg(feature = "download")]
 impl Extras {
@@ -60,7 +61,8 @@ impl Extras {
             let rel = get_github_release(repository.clone());
             if rel.is_some() {
                 let r = rel.unwrap();
-                let u = r.assets.get(0).unwrap().clone().browser_download_url.clone();
+                let filtered = r.assets.into_iter().filter(|a| a.name.to_ascii_lowercase().contains("xxmi-package")).collect::<Vec<&Asset>>();
+                let u = filtered.get(0).unwrap().clone().browser_download_url.clone();
                 let mut downloader = Downloader::new(u).unwrap();
                 let dl = downloader.download(d.join("xxmi.zip").to_path_buf(), |_, _| {});
 
