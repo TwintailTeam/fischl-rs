204e2fd90402228b283fa0cff3934b0c52df6fef
TukanDev <contact@tukandev.com>
2025-10-03 01:17:27 +0200

    Add steamrt download function
    Signed-off-by: TukanDev <contact@tukandev.com>

--- src/compat/mod.rs
+++ src/compat/mod.rs
@@ -1,3 +1,6 @@
+use std::path::PathBuf;
+use crate::utils::downloader::Downloader;
+use crate::utils::extract_archive;
 #[cfg(feature = "compat")]
 use wincompatlib::wine::Wine;
 
@@ -9,4 +12,19 @@ pub mod dxvk;
 #[cfg(feature = "compat")]
 pub struct Compat {
     pub wine: Wine,
+}
+
+#[cfg(feature = "compat")]
+pub fn download_steamrt(path: PathBuf, dest: PathBuf, edition: String) -> bool {
+    if !path.exists() || edition.is_empty() { false } else {
+        let url = format!("https://repo.steampowered.com/steamrt-images-{edition}/snapshots/latest-container-runtime-public-beta/SteamLinuxRuntime_{edition}.tar.xz");
+        let dl = Downloader::new(url);
+        if dl.is_ok() {
+            let mut dll = dl.unwrap();
+            let p = path.join("steamrt.tar.xz");
+            let dld = dll.download(p.as_path(), |_, _| {});
+            if dld.is_ok() { extract_archive("".to_string(), p.to_str().unwrap().to_string(), dest.to_str().unwrap().to_string(), false); }
+            true
+        } else { false }
+    }
 }
