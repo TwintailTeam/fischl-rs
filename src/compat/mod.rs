3e499f858f099fd450351055114a5ea5ec87a840
TukanDev <contact@tukandev.com>
2025-10-03 01:50:03 +0200

    Add steamrt download function
    Signed-off-by: TukanDev <contact@tukandev.com>

--- src/compat/mod.rs
+++ src/compat/mod.rs
@@ -18,13 +18,10 @@ pub struct Compat {
 pub fn download_steamrt(path: PathBuf, dest: PathBuf, edition: String) -> bool {
     if !path.exists() || edition.is_empty() { false } else {
         let url = format!("https://repo.steampowered.com/steamrt-images-{edition}/snapshots/latest-container-runtime-public-beta/SteamLinuxRuntime_{edition}.tar.xz");
-        let dl = Downloader::new(url);
-        if dl.is_ok() {
-            let mut dll = dl.unwrap();
-            let p = path.join("steamrt.tar.xz");
-            let dld = dll.download(p.as_path(), |_, _| {});
-            if dld.is_ok() { extract_archive("".to_string(), p.to_str().unwrap().to_string(), dest.to_str().unwrap().to_string(), false); }
-            true
-        } else { false }
+        let mut dl = Downloader::new(url).unwrap();
+        let p = path.join("steamrt.tar.xz");
+        let dld = dl.download(p.as_path(), |_, _| {});
+        if dld.is_ok() { extract_archive("".to_string(), p.to_str().unwrap().to_string(), dest.to_str().unwrap().to_string(), true); }
+        true
     }
 }
