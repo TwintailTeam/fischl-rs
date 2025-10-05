6229eb1b2ef0dbce9ec2d1a921fc8dcfb5694de4
TukanDev <contact@tukandev.com>
2025-10-05 03:15:18 +0200

    Improve steamrt download and updates
    Signed-off-by: TukanDev <contact@tukandev.com>

--- src/compat/mod.rs
+++ src/compat/mod.rs
@@ -15,13 +15,30 @@ pub struct Compat {
 }
 
 #[cfg(feature = "compat")]
-pub fn download_steamrt(path: PathBuf, dest: PathBuf, edition: String) -> bool {
-    if !path.exists() || edition.is_empty() { false } else {
-        let url = format!("https://repo.steampowered.com/steamrt-images-{edition}/snapshots/latest-container-runtime-public-beta/SteamLinuxRuntime_{edition}.tar.xz");
+pub fn download_steamrt(path: PathBuf, dest: PathBuf, edition: String, branch: String, progress: impl Fn(u64, u64) + Send + 'static) -> bool {
+    if !path.exists() || edition.is_empty() || branch.is_empty() { false } else {
+        let url = format!("https://repo.steampowered.com/steamrt-images-{edition}/snapshots/{branch}/SteamLinuxRuntime_{edition}.tar.xz");
         let mut dl = Downloader::new(url).unwrap();
         let p = path.join("steamrt.tar.xz");
-        let dld = dl.download(p.as_path(), |_, _| {});
+        let dld = dl.download(p.as_path(), progress);
         if dld.is_ok() { extract_archive("".to_string(), p.to_str().unwrap().to_string(), dest.to_str().unwrap().to_string(), true); }
         true
+    }
+}
+
+#[cfg(feature = "compat")]
+pub fn check_steamrt_update(edition: String, branch: String) -> Option<String> {
+    if edition.is_empty() || branch.is_empty() { None } else {
+        let url = format!("https://repo.steampowered.com/steamrt-images-{edition}/snapshots/{branch}/VERSION.txt");
+        let req = reqwest::blocking::get(url);
+        match req {
+            Ok(response) => {
+                match response.text() {
+                    Ok(version_text) => { Some(version_text) },
+                    Err(_) => { None },
+                }
+            }
+            Err(_) => { None },
+        }
     }
 }
