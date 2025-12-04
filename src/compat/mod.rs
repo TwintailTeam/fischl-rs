d68f652124c7210c019d3d3343690c5d7c993ad9
TukanDev <contact@tukandev.com>
2025-12-04 02:40:18 +0100

    Use async downloader for steamrt and steamrt desync fix
    Signed-off-by: TukanDev <contact@tukandev.com>

--- src/compat/mod.rs
+++ src/compat/mod.rs
@@ -1,7 +1,8 @@
 #[cfg(feature = "compat")]
 use std::path::PathBuf;
+use std::sync::Arc;
 #[cfg(feature = "compat")]
-use crate::utils::downloader::Downloader;
+use crate::utils::downloader::AsyncDownloader;
 #[cfg(feature = "compat")]
 use crate::utils::extract_archive;
 #[cfg(feature = "compat")]
@@ -18,21 +19,28 @@ pub struct Compat {
 }
 
 #[cfg(feature = "compat")]
-pub fn download_steamrt(path: PathBuf, dest: PathBuf, edition: String, branch: String, progress: impl Fn(u64, u64) + Send + 'static) -> bool {
+pub async fn download_steamrt(path: PathBuf, dest: PathBuf, edition: String, branch: String, progress: impl Fn(u64, u64) + Send + 'static) -> bool {
     if !path.exists() || edition.is_empty() || branch.is_empty() { false } else {
-        let url = format!("https://repo.steampowered.com/steamrt-images-{edition}/snapshots/{branch}/SteamLinuxRuntime_{edition}.tar.xz");
-        let mut dl = Downloader::new(url).unwrap();
-        let p = path.join("steamrt.tar.xz");
-        let dld = dl.download(p.as_path(), progress);
-        if dld.is_ok() { extract_archive("".to_string(), p.to_str().unwrap().to_string(), dest.to_str().unwrap().to_string(), true); }
-        true
+        let code = if edition.as_str() == "steamrt3" { "sniper" } else { "4" };
+        let url = format!("https://repo.steampowered.com/{edition}/images/{branch}/SteamLinuxRuntime_{code}.tar.xz");
+        let cl = AsyncDownloader::setup_client().await;
+        let dl = AsyncDownloader::new(Arc::new(cl), url).await;
+        if dl.is_ok() {
+            let mut d = dl.unwrap();
+            let p = path.join("steamrt.tar.xz");
+            let dld = d.download(p.as_path(), progress).await;
+            if dld.is_ok() {
+                extract_archive("".to_string(), p.to_str().unwrap().to_string(), dest.to_str().unwrap().to_string(), true);
+                true
+            } else { false }
+        } else { false }
     }
 }
 
 #[cfg(feature = "compat")]
 pub fn check_steamrt_update(edition: String, branch: String) -> Option<String> {
     if edition.is_empty() || branch.is_empty() { None } else {
-        let url = format!("https://repo.steampowered.com/steamrt-images-{edition}/snapshots/{branch}/VERSION.txt");
+        let url = format!("https://repo.steampowered.com/{edition}/images/{branch}/VERSION.txt");
         let req = reqwest::blocking::get(url);
         match req {
             Ok(response) => {
