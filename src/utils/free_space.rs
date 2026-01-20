48ca4d5585c30138a7b87e646a2a5961507388bb
TukanDev <contact@tukandev.com>
2026-01-20 18:52:21 +0100

    Use devid for comparing free space on linux
    Signed-off-by: TukanDev <contact@tukandev.com>

--- src/utils/free_space.rs
+++ src/utils/free_space.rs
@@ -2,22 +2,40 @@ use std::path::Path;
 use sysinfo::Disks;
 
 pub fn available(path: impl AsRef<Path>) -> Option<u64> {
-    let mut disks = Disks::new_with_refreshed_list();
-    disks.sort_by(|a, b| {
-        let a = a.mount_point().as_os_str().len();
-        let b = b.mount_point().as_os_str().len();
-        a.cmp(&b).reverse()
-    });
+    #[cfg(target_os = "windows")]
+    {
+        let mut disks = Disks::new_with_refreshed_list();
+        disks.sort_by(|a, b| {
+            let a = a.mount_point().as_os_str().len();
+            let b = b.mount_point().as_os_str().len();
+            a.cmp(&b).reverse()
+        });
 
-    let mut path = path.as_ref().to_owned();
-    while let Ok(target) = path.read_link() { path = target; }
-    for disk in disks.iter() {
-        let mut dp = disk.mount_point().to_path_buf();
-        while let Ok(target) = dp.read_link() { dp = target; }
-        println!("{} | mountpoint: {}", path.display(), dp.display());
-        if path.starts_with(dp) { return Some(disk.available_space()); }
+        let path = path.as_ref().to_owned();
+        for disk in disks.iter() {
+            let dp = disk.mount_point().to_path_buf();
+            if path.starts_with(dp) { return Some(disk.available_space()); }
+        }
+        None
     }
-    None
+
+    #[cfg(target_os = "linux")]
+    {
+        use std::os::linux::fs::MetadataExt;
+        let mut disks = Disks::new_with_refreshed_list();
+        disks.sort_by(|a, b| {
+            let a = a.mount_point().as_os_str().len();
+            let b = b.mount_point().as_os_str().len();
+            a.cmp(&b).reverse()
+        });
+
+        let path = std::fs::metadata(path.as_ref()).ok()?.st_dev();
+        for disk in disks.iter() {
+            let dp = std::fs::metadata(&disk.mount_point()).ok()?.st_dev();
+            if dp == path { return Some(disk.available_space()); }
+        }
+        None
+    }
 }
 
 pub fn is_same_disk(path1: impl AsRef<Path>, path2: impl AsRef<Path>) -> bool {
